8019b91dd0f34dae408ccac04ae9bdf2049fbc10
yusufsaglamm <yusufsaglm3@gmail.com>
2026-05-27 11:16:54 +0300

    Denemeler

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
- 
+ Bu benim ilk projem her şeye sıfırdan başlıyorum hayırlısı
+Hedefim yazılım dünyasında kendimi geliştirmek
--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-const fs=require('fs').promises;
 
-async function veritiGetirveKaydet() {
-    const api="https://jsonplaceholder.typicode.com/posts/1";
+const fs=require('fs').promises;
+const { error } = require('console');
+const readline=require('readline/promises');
+const{stdin:input,stdout:output}=require('process');
+
+async function kullanicidanAl() {
+    const rl=readline.createInterface({input,output});
+    try{
+        console.log("---APİ SORGULAMA SİSTEMİ---");
+        const secilenID=await rl.question("Sorgulamak istediğiniz Post ID sini giriniz(1-100 arası): ");
+        if(secilenID<1 || secilenID>100){
+            console.log("Yanlış değer girdiniz");
+        }
+        else{
+            console.log("Id alındı işleme devam ediliyor");
+            await veritiGetirveKaydet(secilenID);
+        }
+    }
+    catch(hata){
+        console.log("İşlemde hata oluştu oluşan hata: ",hata.message);
+    }
+    finally{
+        rl.close();
+    }
+    
+}
+
+async function veritiGetirveKaydet(id) {
+    const api=`https://jsonplaceholder.typicode.com/posts/${id}`;
     try{
         console.log("İnternetten veri çekme işlemi başlatıldı");
         const cevap=await fetch(api);
         const veri=await cevap.json();
         console.log("Veri çekildi şimdi dosyaya yazılıyor...");
-        await fs.writeFile('veri.json',JSON.stringify(veri,null,2));
+        await fs.writeFile(`veri-${id}.json`,JSON.stringify(veri,null,2));
     }
     catch(hata){
         console.error("Bir şeyler yanlış gitti yakalanan hata: ",hata.message);
     }
     
 }
-veritiGetirveKaydet();
+kullanicidanAl();
